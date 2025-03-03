ed99e8bdbfee98f9a81d028d7c091deb93252afd
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-03-03 15:33:00 +0100

    controller product completo

--- BeviSano/Controllers/ProductController.cs
+++ BeviSano/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeviSano.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace BeviSano.Controllers
 {
@@ -16,9 +18,38 @@ namespace BeviSano.Controllers
             _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
-            return View();
+            var Products = new StoreList();
+            await using (SqlConnection connection = new SqlConnection(_connectionString)) {
+            await connection.OpenAsync();
+                string query = "SELECT * FROM Products";
+                await using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    await using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var product = new Product
+                            {
+                                Id_Product = reader.GetGuid(0),
+                                Name_Product = reader.GetString(1),
+                                Price_Product = reader.GetDecimal(2),
+                                Description_Product = reader.GetString(3),
+                                Stock_Product = reader.GetInt32(4),
+                                Seller_Product = reader.GetString(5),
+                                Sale_Product = reader.GetDecimal(6),
+                                Arrival_Date_Product = reader.GetDateTime(7),
+                                Cover_Product = reader.GetString(8),
+                                Id_Category = reader.GetInt32(9),
+                            }
+                        ;
+                            Products.Products.Add(product);
+                        }
+                    }
+                }
+            }
+                return View(Products);
         }
     }
 }
---
+++ BeviSano/Models/StoreList.cs
@@ -0,0 +1,9 @@
+﻿namespace BeviSano.Models
+{
+    public class StoreList
+    {
+        public List<Product> Products { get; set; } = [];
+
+        
+    }
+}
