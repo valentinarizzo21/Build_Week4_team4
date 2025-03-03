d15595aa5033bb7430bb0c73922373646119644b
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-03-03 15:03:23 +0100

    product completo

---
+++ BeviSano/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace BeviSano.Models
+{
+    public class Product
+    {
+       public Guid Id_Product { get; set; }
+public string Name_Product { get; set; }
+public decimal Price_Product { get; set; }
+public int Stock_Product { get; set; }
+public string Seller_Product { get; set; }
+public decimal Sale_Product { get; set; }
+public DateTime Arrival_Date_Product { get; set; }
+public string Cover_Product { get; set; }
+public int Id_Category { get; set; }
+        public string Description_Product { get; set; }
+    }
+
+    
+}
