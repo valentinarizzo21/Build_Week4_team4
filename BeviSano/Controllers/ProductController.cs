c188527c74372cd275f945a6bef78d35b6c2a940
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-03-03 14:36:43 +0100

    new branch

---
+++ BeviSano/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeviSano.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly string _connectionString;
+
+        public ProductController()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
