3d94567c90c798e7f22790e7ca5b5b2795a8da09
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-03-03 12:48:51 +0100

    Aggiungere i file di progetto.

---
+++ BeviSano/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeviSano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
