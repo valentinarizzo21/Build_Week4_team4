73e1ffd95b1e505ef5559e5a0475ab3ffcd2d31c
Domy15 <domenico2016dellaventura@gmail.com>
2025-03-04 18:21:40 +0100

    login fatto

---
+++ BeviSano/Models/AddAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace BeviSano.Models
+{
+    public class AddAccount
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
